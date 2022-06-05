2c5aca9650c3afa93fbfa2064cae08d76bf609a8
JaeYoung Heo <89104369+heoj10272@users.noreply.github.com>
2022-06-05 12:16:28 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -2,7 +2,6 @@
 using namespace std;
 
 int d[1000005];
-int ans[3];
 
 int main() {
 	ios::sync_with_stdio(0);
@@ -10,20 +9,14 @@ int main() {
 	d[1] = 0;
 
 	for (int i = 2; i <= 1000000; i++) {
-		ans[0] = { 1000000 };
-		ans[1] = { 1000000 };
-		ans[2] = { 1000000 };
+		d[i] = d[i-1] + 1;
 		if (i % 3 == 0) {
-			ans[0] = d[i / 3] + 1;
+			d[i] = min(d[i], d[i / 3] + 1);
 		}
 		if (i % 2 == 0) {
-			ans[1] = d[i / 2] + 1;
+			d[i] = min(d[i], d[i / 2] + 1);
 		}
-		ans[2] = d[i - 1] + 1;
-
-		d[i] = min(min(ans[0], ans[1]), ans[2]);
 	}
-
 	int num;
 	cin >> num;
 
