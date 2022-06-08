2932afbb029ff57edc2b015285fb7f909e044b66
JaeYoung Heo <89104369+heoj10272@users.noreply.github.com>
2022-06-09 02:44:24 +0900

    [Bronze V] Title: 아!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>	
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	string s1, s2;
+
+	cin >> s1 >> s2;
+	if (s1.size() < s2.size()) {
+		cout << "no";
+	}
+	else cout << "go";
+}
