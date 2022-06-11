5f30d6f875308a3a4d42844047472ddb3f205acc
JaeYoung Heo <89104369+heoj10272@users.noreply.github.com>
2022-06-11 22:05:02 +0900

    [Bronze V] Title: 크냐?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>	
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int a, b;
+	while (1) {
+		cin >> a >> b;
+		if (a == 0 && b == 0) {
+			return 0;
+		}
+		if (a > b) {
+			cout << "Yes\n";
+		}
+		else {
+			cout << "No\n";
+		}
+	}
+}
