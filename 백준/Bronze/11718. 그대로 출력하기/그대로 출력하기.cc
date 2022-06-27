5c4a3b6a521d79606fdfffff6658c57b90740617
JaeYoung Heo <89104369+heoj10272@users.noreply.github.com>
2022-06-27 20:12:21 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	string s;
+	while (1) {
+		getline(cin, s);
+		if (s == "") {
+			break;
+		}
+		cout << s << "\n";
+	}
+}
