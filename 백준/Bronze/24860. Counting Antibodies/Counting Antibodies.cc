f74e78e276c170f2a9cbbaf032b88637a1c053e2
JaeYoung Heo <89104369+heoj10272@users.noreply.github.com>
2022-06-30 23:25:51 +0900

    [Bronze V] Title: Counting Antibodies, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24860. Counting Antibodies/Counting Antibodies.cc
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>	
+using namespace std;
+int main() {
+    long long v1, j1, v2, j2, v, d, j;
+    cin >> v1 >> j1 >> v2 >> j2 >> v >> d >> j;
+    cout << (v*j*d)*(v1*j1+v2*j2) << "\n";
+}
