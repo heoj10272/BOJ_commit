79868ef1cc4c5be744f42dc04ae7ba90a7fe535b
JaeYoung Heo <89104369+heoj10272@users.noreply.github.com>
2022-07-04 17:02:04 +0900

    [Gold IV] Title: N-Queen, Time: 1624 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -1,32 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
-bool isused1[40]; // column을 차지하고 있는지
-bool isused2[40]; // / 방향 대각선을 차지하고 있는지
-bool isused3[40]; // \ 방향 대각선을 차지하고 있는지
 
-int cnt = 0;
 int n;
-void func(int cur) { // cur번째 row에 말을 배치할 예정임
-  if (cur == n) { // N개를 놓는데 성공했다면
-    cnt++;
-    return;
-  }
-  for (int i = 0; i < n; i++) { // (cur, i)에 퀸을 놓을 예정
-    if (isused1[i] || isused2[i+cur] || isused3[cur-i+n-1]) // column이나 대각선 중에 문제가 있다면
-      continue;
-    isused1[i] = 1;
-    isused2[i+cur] = 1;
-    isused3[cur-i+n-1] = 1;
-    func(cur+1);
-    isused1[i] = 0;
-    isused2[i+cur] = 0;
-    isused3[cur-i+n-1] = 0;
-  }
+int isused1[20];
+int isused2[30]; // x+y
+int isused3[30]; // x-y+n-1
+int cnt = 0;
+
+void func(int cur) {
+
+    if (cur == n) {
+        cnt++;
+        return;
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (isused1[i] || isused2[i + cur] || isused3[i - cur + n - 1]) continue;
+        isused1[i] = 1;
+        isused2[i + cur] = 1;
+        isused3[i - cur + n - 1] = 1;
+        func(cur + 1);
+        isused1[i] = 0;
+        isused2[i + cur] = 0;
+        isused3[i - cur + n - 1] = 0;
+    }
 }
 int main(void) {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cin >> n;
-  func(0);
-  cout << cnt;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> n;
+    func(0);
+    cout << cnt;
 }
