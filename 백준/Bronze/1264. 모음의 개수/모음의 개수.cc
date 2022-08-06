293485ae51739add1d9a709c625943ab5652c986
JaeYoung Heo <89104369+heoj10272@users.noreply.github.com>
2022-08-06 16:04:39 +0900

    [Bronze IV] Title: 모음의 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	while (1) {
+		int res = 0;
+		string s;
+		getline(cin, s);
+		if (s[0] == '#') {
+			break;
+		}
+		for (int i = 0; i < s.length(); i++) {
+			if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U') {
+				res++;
+			}
+		}
+
+		cout << res << "\n";
+	}
+}
