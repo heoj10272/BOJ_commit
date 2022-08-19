f331ebc888354ccd3b08aecde598fffc9165f7f5
JaeYoung Heo <89104369+heoj10272@users.noreply.github.com>
2022-08-19 15:38:53 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    double a, b;
+    cin >> a >> b;
+
+    printf("%.6f\n", a * 2 + 2 * 3.141592 * b);
+}
